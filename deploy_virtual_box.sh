1f34cab6cc4ffaea2fab0c0a66eccafcb3e281fb
q191201771 <191201771@.qq.com>
2016-08-27 11:27:48 +0800

    使用rsync部署虚拟机

--- deploy_virtual_box.sh
+++ deploy_virtual_box.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sh clean.sh
-tar zcvf ../starry-night.tgz ../starry-night
-scp -P 4022 ../starry-night.tgz chef@localhost:~/
-ssh -p 4022 chef@localhost "cd /home/chef && tar zxvf starry-night.tgz && cd /home/chef/starry-night && scons && ./unit_test.sh"
+rsync -a -e 'ssh -p 4022' --progress . --exclude .git localhost:/tmp/starry-night
+ssh -p 4022 chef@localhost "cd /tmp/starry-night && scons && ./unit_test.sh"
+
