dd1b4e29d1cb341144728b2496c1b11fc7612321
q191201771 <191201771@qq.com>
2016-12-15 11:19:22 +0800

    bugfix assert_wrapper.hpp

--- deploy_virtual_box.sh
+++ deploy_virtual_box.sh
@@ -4,3 +4,4 @@ sh clean.sh
 rsync -rlpgoDvP -e 'ssh -p 4022' --progress . --exclude .git chef@localhost:/tmp/starry-night
 #ssh -p 4022 chef@localhost "cd /tmp/starry-night && scons mode=debug && ./unit_test.sh"
 ssh -p 4022 chef@localhost "cd /tmp/starry-night && scons mode=debug"
+#ssh -p 4022 chef@localhost "cd /tmp/starry-night && scons"
--- src/base_test/common/assert_wrapper.hpp
+++ src/base_test/common/assert_wrapper.hpp
@@ -7,7 +7,9 @@
  *     -initial release xxxx-xx-xx
  *
  * @brief
- *   重定义debug模式时，assert的行为（由coredump改为往stderr输出日志）。release模式下依然保持原有逻辑（忽略assert）
+ *   重定义assert的行为（由coredump改为往stderr输出日志）。
+ *   使用场景：做ut时，希望assert失败后只爆出一条日志，程序继续执行后续的case。
+ *   请勿在生产环境直接使用该wrapper！
  *
  */
 
@@ -15,9 +17,9 @@
 #define _BASE_TEST_COMMON_ASSERT_WRAPPER_HPP_
 
 #include <assert.h>
-#ifdef NDEBUG
+//#ifndef NDEBUG
 #undef assert
 #define assert(expr) if(!(expr)) {fprintf(stderr, "%s %s %d\n", #expr, __FILE__, __LINE__);};
-#endif
+//#endif
 
 #endif // _BASE_TEST_COMMON_ASSERT_WRAPPER_HPP_
