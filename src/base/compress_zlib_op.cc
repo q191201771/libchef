8d7523fc884635b8f019d5ed6a2129e36a9b0bc3
q191201771 <191201771@qq.com>
2017-01-02 10:46:45 +0800

    添加compress_zlib_op

--- src/base/compress_zlib_op.cc
+++ src/base/compress_zlib_op.cc
@@ -1,1 +1,167 @@
 #include "compress_zlib_op.h"
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#include <zlib.h>
+
+static const int CHUNK_LEN = 8192;
+
+namespace chef {
+
+  int compress_zlib_op::compress(const unsigned char *in, int in_len, unsigned char **out, int *out_len,
+                                 COMPRESSION_LEVEL level)
+  {
+    int ret, flush;
+    int have;
+    z_stream strm;
+    unsigned char fixed_chunk[CHUNK_LEN];
+
+    strm.zalloc = Z_NULL;
+    strm.zfree  = Z_NULL;
+    strm.opaque = Z_NULL;
+    ret = deflateInit(&strm, level);
+    if (ret != Z_OK) {
+      return -1;
+    }
+
+    int out_data_capacity = in_len;
+    unsigned char *out_data = (unsigned char *)malloc(out_data_capacity);
+
+    int left         = in_len;
+    int used         = 0;
+    int out_data_len = 0;
+
+    do {
+      strm.avail_in = left > CHUNK_LEN ? CHUNK_LEN : left;
+      flush = left <= CHUNK_LEN ? Z_FINISH : Z_NO_FLUSH;
+      strm.next_in = (unsigned char *)in + used;
+      used += strm.avail_in;
+      left -= strm.avail_in;
+
+      do {
+        strm.avail_out = CHUNK_LEN;
+        strm.next_out  = fixed_chunk;
+        ret = deflate(&strm, flush);
+        if (ret == Z_STREAM_ERROR) {
+          goto cleanup;
+        }
+        have = CHUNK_LEN - strm.avail_out;
+
+        if (out_data_len + have > out_data_capacity) {
+          out_data_capacity = out_data_len + have;
+          out_data_capacity <<= 1;
+          unsigned char *tmp = (unsigned char *)realloc(out_data, out_data_capacity);
+          if (tmp == NULL) {
+            goto cleanup;
+          }
+          out_data = tmp;
+        }
+
+        memcpy(out_data+out_data_len, fixed_chunk, have);
+        out_data_len += have;
+      } while (strm.avail_in == 0);
+    } while (flush != Z_FINISH);
+
+    if (ret != Z_STREAM_END) {
+      goto cleanup;
+    }
+
+    *out     = out_data;
+    *out_len = out_data_len;
+
+    (void)deflateEnd(&strm);
+    return 0;
+
+  cleanup:
+    (void)deflateEnd(&strm);
+    if (out_data != NULL) {
+      free(out_data);
+    }
+    return -1;
+  }
+
+  int compress_zlib_op::decompress(const unsigned char *in, int in_len, unsigned char **out, int *out_len) {
+    int ret;
+    int have;
+    z_stream strm;
+
+    unsigned char fixed_chunk[CHUNK_LEN];
+
+    strm.zalloc   = Z_NULL;
+    strm.zfree    = Z_NULL;
+    strm.opaque   = Z_NULL;
+    strm.avail_in = 0;
+    strm.next_in  = Z_NULL;
+    ret = inflateInit(&strm);
+    if (ret != Z_OK) {
+      return -1;
+    }
+
+    int out_data_capacity = in_len << 2;
+    unsigned char *out_data = (unsigned char *)malloc(out_data_capacity);
+
+    int left = in_len;
+    int used = 0;
+    int out_data_len = 0;
+
+    do {
+      strm.avail_in = left > CHUNK_LEN ? CHUNK_LEN : left;
+      if (strm.avail_in <= 0) {
+        break;
+      }
+
+      strm.next_in = (unsigned char *)in + used;
+      used += strm.avail_in;
+      left -= strm.avail_in;
+
+      do {
+        strm.avail_out = CHUNK_LEN;
+        strm.next_out  = fixed_chunk;
+        ret = inflate(&strm, Z_NO_FLUSH);
+        //assert(ret != Z_STREAM_ERROR);
+        switch (ret) {
+          case Z_STREAM_ERROR:
+          case Z_NEED_DICT:
+          case Z_DATA_ERROR:
+          case Z_MEM_ERROR:
+            goto cleanup;
+        }
+        have = CHUNK_LEN - strm.avail_out;
+
+        if (out_data_len + have > out_data_capacity) {
+          out_data_capacity = out_data_len + have;
+          out_data_capacity <<= 1;
+          unsigned char *tmp = (unsigned char *)realloc(out_data, out_data_capacity);
+          if (tmp == NULL) {
+            goto cleanup;
+          }
+
+          out_data = tmp;
+        }
+
+        memcpy(out_data+out_data_len, fixed_chunk, have);
+        out_data_len += have;
+      } while (strm.avail_out == 0);
+    } while (ret != Z_STREAM_END);
+
+    if (ret != Z_STREAM_END) {
+      goto cleanup;
+    }
+
+    *out_len = out_data_len;
+    *out = out_data;
+
+    (void)inflateEnd(&strm);
+    return 0;
+
+  cleanup:
+      (void)inflateEnd(&strm);
+      if (out_data) {
+          free(out_data);
+      }
+      return -1;
+  }
+
+} // namespace chef
+
--- src/base/compress_zlib_op.h
+++ src/base/compress_zlib_op.h
@@ -9,29 +9,54 @@
  * @brief
  *   压缩、解压zlib流
  *
+ * @NOTICE 实现参考了zlib的官方例子 `zlib/example/zpipe.c`
+ *
  */
 
 #ifndef _CHEF_BASE_COMPRESS_ZLIB_OP_H_
 #define _CHEF_BASE_COMPRESS_ZLIB_OP_H_
 
-#include <string>
-
 namespace chef {
 
   class compress_zlib_op {
     public:
-      int compress(const std::string &in, std::string &out);
+      enum COMPRESSION_LEVEL {
+        COMPRESSION_LEVEL_DEFAULT          = -1, /// 默认
+        COMPRESSION_LEVEL_BEST_SPEED       =  1, /// 最快
+        COMPRESSION_LEVEL_BEST_COMPRESSION =  9, /// 压缩率最高
+      };
 
-      int uncompress(const std::string &in, std::string &out);
-  };
-
-  class compress_gzip_op {
     public:
-      int compress(const std::string &in, std::string &out);
+      /**
+       * zlib压缩
+       *
+       * @param      in 待压缩的数据
+       * @param  in_len 待压缩数据的长度
+       * @param     out 传出参数，压缩后的数据（内存由内部申请，外部用完后手动调用free释放）
+       * @param out_len 传出参数，压缩后数据的长度
+       * @param   level 压缩率，取值参见enum COMPRESSION_LEVEL
+       *
+       * @return 0 成功 -1 失败
+       */
+      int compress(const unsigned char *in, int in_len, unsigned char **out, int *out_len, COMPRESSION_LEVEL level);
 
-      int uncompress(const std::string &in, std::string &out);
+      /**
+       * zlib解压
+       *
+       * @param      in 待解压的数据
+       * @param  in_len 待解压数据的长度
+       * @param     out 传出参数，解压后的数据（内存由内部申请，外部用完后手动调用free释放）
+       * @param out_len 传出参数，解压后数据的长度
+       *
+       * @return 0 成功 -1 失败
+       *
+       */
+      int decompress(const unsigned char *in, int in_len, unsigned char **out, int *out_len);
   };
+
+  /// TODO class compress_gzip_op
 
 } /// namespace chef
 
 #endif /// _CHEF_BASE_COMPRESS_ZLIB_OP_H_
+
