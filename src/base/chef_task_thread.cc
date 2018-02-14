851613b586cecb2657e8af6181f9ff66a667502f
pengrl <pengrl@inke.cn>
2018-02-14 10:20:36 +0800

    task thread self impl clock_gettime at macos

--- src/base/chef_task_thread.cc
+++ src/base/chef_task_thread.cc
@@ -114,6 +114,23 @@ namespace chef {
     }
   }
 
+// https://stackoverflow.com/questions/5167269/clock-gettime-alternative-in-mac-os-x
+#if defined(__MACH__) && !defined(CLOCK_REALTIME)
+#include <sys/time.h>
+#define CLOCK_REALTIME 0
+#define CLOCK_MONOTONIC 0
+// clock_gettime is not implemented on older versions of OS X (< 10.12).
+// If implemented, CLOCK_REALTIME will have already been defined.
+int clock_gettime(int /*clk_id*/, struct timespec* t) {
+    struct timeval now;
+    int rv = gettimeofday(&now, NULL);
+    if (rv) return rv;
+    t->tv_sec  = now.tv_sec;
+    t->tv_nsec = now.tv_usec * 1000;
+    return 0;
+}
+#endif
+
   void task_thread::execute_tasks_(std::multimap<uint64_t, task> &tasks) {
 //    for (auto item : tasks) {
     std::multimap<uint64_t, task>::iterator iter = tasks.begin();
