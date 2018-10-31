f42bbd4534d55caa0b2cd5996f81c5241c84b76b
q191201771 <191201771@qq.com>
2018-10-31 15:03:17 +0800

    add cmake

--- ut.sh
+++ ut.sh
@@ -1,37 +1,35 @@
-rm -rf output && \
-scons -c && \
-scons -j8 mode=debug && \
+#/bin/bash
+
+./clean.sh && \
+./build.sh
 echo '----------------------------------------' && \
-./output/chef_base_test/chef_buffer_test && \
-./output/chef_base_test/chef_consistent_hash_test && \
-./output/chef_base_test/chef_count_dump_test && \
-./output/chef_base_test/chef_crypto_md5_op_test && \
-./output/chef_base_test/chef_crypto_sha1_op_test && \
-./output/chef_base_test/chef_defer_test && \
-./output/chef_base_test/chef_encoding_base64_op_test && \
-./output/chef_base_test/chef_env_var_op_test && \
-./output/chef_base_test/chef_filepath_op_test && \
-./output/chef_base_test/chef_fmt_op_test && \
-./output/chef_base_test/chef_hash_test && \
-./output/chef_base_test/chef_lru_test && \
-./output/chef_base_test/chef_os_exec_op_test && \
-./output/chef_base_test/chef_skiplist_test && \
-./output/chef_base_test/chef_snippet_test && \
-./output/chef_base_test/chef_stringify_stl_test && \
-./output/chef_base_test/chef_strings_op_test && \
-./output/chef_base_test/chef_task_thread_test && \
-./output/chef_base_test/chef_this_machine_op_test && \
-./output/chef_base_test/chef_this_process_op_test && \
-./output/chef_base_test/chef_thread_group_test && \
-./output/chef_base_test/chef_thread_pool_test && \
-./output/chef_base_test/chef_wait_event_test && \
-./output/chef_base_test/chef_wait_event_counter_test && \
-./output/chef_base_test/chef_weight_selector_test && \
+./output/bin/chef_buffer_test && \
+./output/bin/chef_consistent_hash_test && \
+./output/bin/chef_count_dump_test && \
+./output/bin/chef_crypto_md5_op_test && \
+./output/bin/chef_crypto_sha1_op_test && \
+./output/bin/chef_defer_test && \
+./output/bin/chef_encoding_base64_op_test && \
+./output/bin/chef_env_var_op_test && \
+./output/bin/chef_filepath_op_test && \
+./output/bin/chef_fmt_op_test && \
+./output/bin/chef_hash_test && \
+./output/bin/chef_lru_test && \
+./output/bin/chef_os_exec_op_test && \
+./output/bin/chef_skiplist_test && \
+./output/bin/chef_snippet_test && \
+./output/bin/chef_stringify_stl_test && \
+./output/bin/chef_strings_op_test && \
+./output/bin/chef_task_thread_test && \
+./output/bin/chef_this_machine_op_test && \
+./output/bin/chef_this_process_op_test && \
+./output/bin/chef_thread_group_test && \
+./output/bin/chef_thread_pool_test && \
+./output/bin/chef_wait_event_test && \
+./output/bin/chef_wait_event_counter_test && \
+./output/bin/chef_weight_selector_test && \
 rm -rf chef_thread_group_test.dump && \
 rm -rf not_exist && \
 echo '----------------------------------------' && \
 echo 'all unit test succ. :)'
-
-#./output/base_test/chef_http_op_test
-#./output/base_test/chef_log_test
 
