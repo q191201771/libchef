99e41cc344d5ac131ba40bdd72837771bae13685
q191201771 <191201771@qq.com>
2018-12-26 10:26:20 +0800

    header brief @git -> @license

--- include/chef_base/chef_thread_pool.hpp
+++ include/chef_base/chef_thread_pool.hpp
@@ -1,5 +1,5 @@
 /**
- * @git      https://github.com/q191201771/starry-night
+ * @license  this file is part of starry-night library. more info see https://github.com/q191201771/starry-night
  * @tag      v1.7.17
  * @file     chef_thread_pool.hpp
  * @deps     chef_env.hpp | chef_wait_event_counter.hpp
